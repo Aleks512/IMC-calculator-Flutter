8f0c6bce57335c787a53e8e87a04658fc499791d
Aleks512 <aleksandracichocka999@gmail.com>
2024-03-07 09:05:22 +0100

    refactoring

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'custom_col_content.dart';
 import 'reusable_cart.dart';
 
-const bottomContainerHeight = 80.0;
-const bottomContainerColor = Color.fromARGB(255, 224, 175, 41);
-const reusableCardsColor = Color(0xFF1D1E33);
 
-const maleIcon = FontAwesomeIcons.mars;
-const femaleIcon = FontAwesomeIcons.venus;
-const femaleText = "FEMME";
-const maleText = "HOMME";
+const bottomContainerColor = Color.fromARGB(255, 224, 175, 41);
+
 
 class InputPage extends StatefulWidget {
   const InputPage({super.key});
