c06d5d40cd5cb6d338879d58511d0790db006b39
Aleks512 <aleksandracichocka999@gmail.com>
2024-03-13 16:56:08 +0100

    3 - builder: () {}

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -200,7 +200,7 @@ class _InputPageState extends State<InputPage> {
           ),
           GestureDetector(
             onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: builder))
+              Navigator.push(context, MaterialPageRoute(builder: () {}))
             },
             child: Container(
               child: Text('CALCULER'),
